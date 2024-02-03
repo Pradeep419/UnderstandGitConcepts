5acfbd13bca872d5eb9f2e1712378f964b5c2066
Pradeep419 <g.pradeep419@gmail.com>
2024-02-03 10:33:40 +0530

    First Commit

---
+++ typescript-learn.js
@@ -0,0 +1,5 @@
+function add(num1, num2) {
+    return (num1 + num2);
+}
+var num3 = add(3, 4);
+console.log(num3);
---
+++ typescript-learn.ts
@@ -0,0 +1,6 @@
+function add(num1:number, num2:number):number{
+    return(num1+num2);
+}
+
+let num3 = add(3,4);
+console.log(num3);
