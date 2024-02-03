0c9508a9b1de5cf6c974e16829e1cb34aa6cfb65
Pradeep419 <g.pradeep419@gmail.com>
2024-02-03 10:58:52 +0530

    Second Commit

---
+++ typescript-learn-1.js
@@ -0,0 +1,5 @@
+function subtract(num1, num2) {
+    return (num1 + num2);
+}
+var sub = subtract(3, 4);
+console.log(sub);
---
+++ typescript-learn-1.ts
@@ -0,0 +1,6 @@
+function subtract(num1:number, num2:number):number{
+    return(num1+num2);
+}
+
+let sub = subtract(3,4);
+console.log(sub);
